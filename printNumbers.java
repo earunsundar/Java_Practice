62fa254fc70b031b85a486eb4e61603fbdf400dc
ArunSundar <35741503+earunsundar@users.noreply.github.com>
2021-07-30 00:10:20 +0530

    Add files via upload

---
+++ printNumbers.java
@@ -0,0 +1,14 @@
+package samplePrograms;
+
+public class printNumbers {
+    // Print numbers 1 to 100 without using integers or numbers
+
+
+    public static void main(String[] args) {
+        int one ='A'/'A'; // the vaklue is 1
+        String value = "..........";
+        for (int i=one;i<=(value.length()*value.length());i++){
+            System.out.println(i);
+        }
+    }
+}
